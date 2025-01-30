673a33fa87b3181c6734c950d85b2c3ab88dc1e4
DaffafauzanD <daffafauzand24@gmail.com>
2025-01-30 20:26:41 +0700

    Modify main.py styles and base HTML template

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, redirect
+import pickle
+import sklearn
+import numpy as np     
 import os
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if request.method == 'POST':
 
+        with open('knn_pickle', 'rb') as r:
+            model = pickle.load(r)
+
+        melahirkan = float(request.form['melahirkan'])
+        glukosa = float(request.form['glukosa'])
+        darah = float(request.form['darah'])
+        kulit = float(request.form['kulit'])
+        insulin = float(request.form['insulin'])
+        bmi = float(request.form['bmi'])
+        riwayat = float(request.form['riwayat'])
+        umur = float(request.form['umur'])
+
+        datas = np.array((melahirkan,glukosa,darah,kulit,insulin,bmi,riwayat,umur))
+        datas = np.reshape(datas, (1, -1))
+
+        isDiabetes = model.predict(datas)
+
+        return render_template('hasil.html', finalData=isDiabetes)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
